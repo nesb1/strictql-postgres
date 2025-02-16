6fcde3fd35584b65ce7ba62a1f4f1dd9a24b964b
Булыгин Евгений Андреевич <bulygin.e@spectrumdata.ru>
2025-02-16 15:20:47 +0500

    Добавил соединение ранее написанного кода с api

--- strictql_postgres/__main__.py
+++ strictql_postgres/__main__.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass
 from cyclopts import App, Parameter
 from typing import Annotated, Literal
 
+from strictql_postgres.code_generator import generate_code_for_query, QueryWithDBInfo
+from strictql_postgres.code_quality import CodeQualityImprover, MypyRunner
+from strictql_postgres.string_in_snake_case import StringInSnakeLowerCase
+
 app = App()
 
 
@@ -20,8 +24,18 @@ async def generate(
     Команда будет искать настройки `strictql` в файле `pyproject.toml`, если файла или настроек нет, то произойдет ошибка.
     """
 
-    print(dry_run)
-    raise NotImplementedError()
+    print(
+        await generate_code_for_query(
+            query_with_db_info=QueryWithDBInfo(
+                query="select 1 as v", query_result_row_model={"v": int}
+            ),
+            execution_variant="fetch_all",
+            function_name=StringInSnakeLowerCase("select_1"),
+            code_quality_improver=CodeQualityImprover(
+                mypy_runner=MypyRunner(mypy_path=pathlib.Path(__file__).parent)
+            ),
+        )
+    )
 
 
 @app.command()  # type: ignore[misc] # Expression contains "Any", todo fix it on cyclopts
